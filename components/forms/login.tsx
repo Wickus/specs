f95fa78e317484d84fe53879bc857c26358de0e8
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-06-10 16:07:45 +0200

    HOTFIX: Fixed the export error in components/forms/login.tsx

--- components/forms/login.tsx
+++ components/forms/login.tsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 import { login } from "@/lib/forms/login";
 import { useRouter } from "next/router";
 
-export default () => {
+const LoginForm = () => {
     const inputStyles = "border-[1px] w-full p-2 border-gray-300 rounded-md mb-5";
-	const router = useRouter();
+    const router = useRouter();
 
     const onSubmit = () => {
-        login.then((res:any) => {
-            if(res.success){
-				router.push("/");
-			}
+        login.then((res: any) => {
+            if (res.success) {
+                router.push("/");
+            }
         });
     };
 
     return (
         <div className="max-w-[500px] w-full p-10 shadow-lg shadow-black-1/2 rounded-md bg-white">
             <div className="max-w-[160px] mx-auto mb-5">
-                <Image src="/logo.png" width={1366} height={660} />
+                <Image src="/logo.png" width={1366} height={660} alt="Specs Logo" />
             </div>
             <form
                 className="flex flex-col items-center justify-center"
@@ -40,3 +40,5 @@ export default () => {
         </div>
     );
 };
+
+export default LoginForm;
