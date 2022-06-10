0e3f7175ead4887f51bb78ce6445d062de76052a
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-06-10 21:51:53 +0200

    Created the home page action bar UI (#6) (#7)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import ActionBar from '@/components/actionbar'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Home Page
+    <div className='bg-gradient-to-r from-gray-900 bg-gray-800 w-full h-full'>
+      <ActionBar/>
     </div>
   )
 }
