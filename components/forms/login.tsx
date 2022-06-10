189184931378d9dd908463a2afea83465742bbea
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-06-10 15:55:32 +0200

    Release v0.1.0 (#4)
    * FEATURE: Add dev dependencies (#1)
    
    * Added the style dependancies.
    
    * Configured Tailwind for the project.
    Updated css files to scss.
    
    * Created the login screen (#3)
    
    - Added Username & password inputs
    - Created files for login logic
    - Added routing when login is successful

---
+++ components/forms/login.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { login } from "@/lib/forms/login";
+import { useRouter } from "next/router";
+
+export default () => {
+    const inputStyles = "border-[1px] w-full p-2 border-gray-300 rounded-md mb-5";
+	const router = useRouter();
+
+    const onSubmit = () => {
+        login.then((res:any) => {
+            if(res.success){
+				router.push("/");
+			}
+        });
+    };
+
+    return (
+        <div className="max-w-[500px] w-full p-10 shadow-lg shadow-black-1/2 rounded-md bg-white">
+            <div className="max-w-[160px] mx-auto mb-5">
+                <Image src="/logo.png" width={1366} height={660} />
+            </div>
+            <form
+                className="flex flex-col items-center justify-center"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit();
+                }}
+            >
+                <input className={inputStyles} type="text" name="username" placeholder="Username" />
+                <input className={inputStyles} type="password" name="password" placeholder="Password" />
+                <div className="flex items-center justify-between w-full mt-5">
+                    <button type="submit" className="bg-gray-300 w-[180px] h-[40px] rounded-md">
+                        Login
+                    </button>
+                    <button type="button" className="bg-gray-300 w-[180px] h-[40px] rounded-md">
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
---
+++ lib/forms/login.ts
@@ -0,0 +1,5 @@
+export const login = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ success: true });
+    }, 1000);
+});
