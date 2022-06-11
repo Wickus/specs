6052fbd52ddde02905ee3ed32f348c9197c69305
Wickus Van Der Merwe <vandermerwewickus@gmail.com>
2022-06-11 11:26:53 +0200

    Release v0.2.0 (#10)
    * Created the home page action bar UI (#6)
    
    * FEATURE: Create media view UI (#9)
    
    * Created the home page action bar UI (#6) (#7)
    
    * Added the category UI and Media display UI (#8)
    
    * Created the media views for movies and series

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ const Home: NextPage = () => {
   return (
     <div className='bg-gradient-to-r from-gray-900 bg-gray-800 w-full h-full max-h-full overflow-y-auto'>
       <ActionBar/>
-	  <Categories/>
+	    <Categories/>
+	    <Categories/>
     </div>
   )
 }
